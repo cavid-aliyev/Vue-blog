05a1c32ad2e284bbec7a0ef80dfb2fd6547f8f83
Alyeev <jaliyev@conceptsoft.io>
2022-03-17 12:31:14 +0400

    Add pagination && fix fetch issue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app">
     <h1>Post Page</h1>
-    <Input v-model="searchQuery" />
+    <Input :type="'text'" v-model="searchQuery" placeholder="Search..." />
     <div class="app_btns">
       <CustomButton @click="showModal">Create post</CustomButton>
       <PostSelect v-model="selectedSort" :options="sortOptions" />
@@ -12,11 +12,25 @@
     </PostModal>
 
     <PostList
-      :posts="sortedPosts"
+      :posts="sortedAndSearchedPosts"
       @remove="removePost"
       v-if="!isPostsLoading"
     />
     <h2 v-else>Loading</h2>
+    <!-- Pagination -->
+    <div class="page__wrapper">
+      <div
+        v-for="pageNum in totalPages"
+        :key="pageNum"
+        class="page"
+        :class="{
+          current_page: page === pageNum,
+        }"
+        @click="changePage(pageNum)"
+      >
+        {{ pageNum }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,6 +51,9 @@ export default {
       isPostsLoading: false,
       selectedSort: "",
       searchQuery: "",
+      totalPages: 0,
+      page: 1,
+      limit: 10,
       sortOptions: [
         { value: "title", name: "By name" },
         { value: "body", name: "By descr" },
@@ -54,11 +71,24 @@ export default {
     showModal() {
       this.modalVisible = true;
     },
+    changePage(pageNum) {
+      this.page = pageNum;
+      // this.fetchPosts();
+    },
     async fetchPosts() {
       try {
         this.isPostsLoading = true;
         const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        );
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
         );
         this.posts = response.data;
       } catch (error) {
@@ -79,17 +109,26 @@ export default {
         );
       });
     },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter((post) =>
+        post.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    },
   },
-  // watch: {
-  //   // should be equal name with data
-  //   selectedSort(newValue) {
-  //     newValue === "id"
-  //       ? this.posts.sort((post1, post2) => post1.id - post2.id)
-  //       : this.posts.sort((post1, post2) => {
-  //           return post1[newValue]?.localeCompare(post2[newValue]);
-  //         });
-  //   },
-  // },
+  watch: {
+    page() {
+      this.fetchPosts();
+    },
+
+    //   // should be equal name with data
+    //   selectedSort(newValue) {
+    //     newValue === "id"
+    //       ? this.posts.sort((post1, post2) => post1.id - post2.id)
+    //       : this.posts.sort((post1, post2) => {
+    //           return post1[newValue]?.localeCompare(post2[newValue]);
+    //         });
+    //   },
+  },
 };
 </script>
 
@@ -109,5 +148,19 @@ export default {
   display: flex;
   justify-content: space-between;
   margin: 15px 0;
+}
+
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+
+.current_page {
+  border: 2px solid teal;
 }
 </style>
